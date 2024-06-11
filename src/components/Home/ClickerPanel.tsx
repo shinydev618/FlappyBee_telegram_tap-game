09d673fb67dfe0b630fffbf64652baff329eb5fe
shinydev618 <shinydev618@gmail.com>
2024-06-11 16:27:39 -0400

    updated clicking event

--- src/components/Home/ClickerPanel.tsx
+++ src/components/Home/ClickerPanel.tsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 import { BOOSTER_IMAGE } from "../../constants/image";
 import Energy from "../svgs/Energy";
+import { useState } from "react";
 
 const ClickerPanel = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="flex flex-col mt-5 relative touch-none">
-      <button className="flex bg-transparent h-[300px] w-full mx-auto">
+      <button
+        className="flex bg-transparent h-[300px] w-full mx-auto"
+        onClick={() => {
+          let temp = count;
+          temp++;
+          setCount(temp);
+        }}
+      >
         <div className="mx-auto bg-[linear-gradient(180deg,#5a60ff,#3b40934f_49.53%,#1c1f24)] rounded-full h-[300px] p-[15px] w-[300px] relative before:absolute before:content-[''] before:border before:border-[rgba(90,96,255,.15)] before:rounded-full before:top-0 before:left-0 before:w-full before:h-full">
           <div className="flex justify-center items-center border border-[rgba(39,39,39,0)] rounded-full shadow-[0_0_15px_#304669] h-full w-full z-[3] bg-[#272a2f] relative before:absolute before:content-[''] before:bg-[radial-gradient(circle,#3763ff_0,#3763ff00_80%)] before:top-0 before:left-0 before:w-full before:h-full before:rounded-full">
             <img
@@ -20,7 +30,7 @@ const ClickerPanel = () => {
       <div className="flex justify-between items-center mb-2">
         <div className="flex items-center">
           <Energy className="w-[26px] h-[26px] mr-1.5" />
-          <span className="font-bold">1500 / 1500</span>
+          <span className="font-bold">{count} / 1500</span>
         </div>
         <Link
           to={"/boost"}
